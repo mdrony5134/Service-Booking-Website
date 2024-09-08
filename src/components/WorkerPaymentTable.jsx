03a90aac6a465d330eaccd89317f6e4f1b256798
mdrony5134 <rony830388@gmail.com>
2024-09-08 12:10:07 +0600

    workerProfile updated and workerPaymentTable added

---
+++ src/components/WorkerPaymentTable.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import { MdClose } from "react-icons/md";
+
+const WorkerPaymentTable = ({ handleCloseService }) => {
+  return (
+    <div className=" bg-blueGray-50 py-10 md:pt-60 md:pb-20 flex justify-center items-center fixed left-0 top-0 z-[1055] bg-black bg-opacity-65 h-full w-full overflow-y-auto overflow-x-hidden outline-none">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="flex flex-wrap md:w-full items-center bg-white shadow-md py-5 w-[190%]">
+          <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+            <h3 className="font-semibold text-lg text-blueGray-700">
+              Worker payment details
+            </h3>
+          </div>
+          <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+            <button onClick={handleCloseService} className="text-3xl">
+              <MdClose />
+            </button>
+          </div>
+        </div>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Service name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Payment
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Price
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Apple MacBook Pro 17"
+              </th>
+              <td className="px-6 py-4">Silver</td>
+              <td className="px-6 py-4">Laptop</td>
+              <td className="px-6 py-4">$2999</td>
+              <td className="px-6 py-4">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Microsoft Surface Pro
+              </th>
+              <td className="px-6 py-4">White</td>
+              <td className="px-6 py-4">Laptop PC</td>
+              <td className="px-6 py-4">$1999</td>
+              <td className="px-6 py-4">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Magic Mouse 2
+              </th>
+              <td className="px-6 py-4">Black</td>
+              <td className="px-6 py-4">Accessories</td>
+              <td className="px-6 py-4">$99</td>
+              <td className="px-6 py-4">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default WorkerPaymentTable;
--- src/pages/WorkerProfile.jsx
+++ src/pages/WorkerProfile.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import WorkerService from "../components/WorkerService";
+import WorkerPaymentTable from "../components/WorkerPaymentTable";
 
 const WorkerProfile = () => {
   const [workerServiceModal, setWorkerServiceModal] = useState(false);
 
-  const handleService = () => {
-    setWorkerServiceModal(true);
+  const handleService = (event) => {
+    setWorkerServiceModal(event.target.value);
   };
   const handleCloseService = () => {
     setWorkerServiceModal(false);
@@ -29,25 +30,31 @@ const WorkerProfile = () => {
                       className="shadow-xl rounded-full h-auto align-middle bg-blue-600 border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]"
                       alt=""
                     />
-                  </div>
-                  <div className="md:left-52 left-24 relative top-4">
-                    <button
-                      onClick={handleService}
-                      className=" text-white bg-[#41B3A2] hover:bg-blue-500 font-medium rounded-3xl text-sm px-5 py-2.5 text-center "
-                    >
-                      Your Service
-                    </button>
+                    <div className="md:left-24 top-28 -left-24 absolute md:top-4">
+                      <select
+                        onChange={handleService}
+                        value={workerServiceModal}
+                        className=" text-white bg-[#41B3A2] font-medium rounded-md text-sm px-5 py-2.5 text-center mr-8 focus:outline-none"
+                      >
+                        <option value="">Select Service Info</option>
+                        <option value="details">Service Details</option>
+                        <option value="payment">Service Payment</option>
+                      </select>
+                    </div>
                   </div>
                 </div>
 
-                <div className="text-start   mt-28">
+                <div className="text-start mt-44   md:mt-28">
                   <h3 className="text-xl font-semibold leading-normal text-blueGray-700 mb-2">
-                    Name : <span className="text-xl font-normal">Mr.Jasim</span>
+                    Name :{" "}
+                    <span className="text-xl font-normal italic">Mr.Jasim</span>
                   </h3>
                   <div className="text-xl leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
                     <h3 className="text-xl font-semibold leading-normal text-blueGray-700 mb-2">
                       Phone :{" "}
-                      <span className="text-xl font-normal">017XXXXXXXX</span>
+                      <span className="text-xl font-normal italic">
+                        017XXXXXXXX
+                      </span>
                     </h3>
                   </div>
                   <div className="text-xl leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
@@ -55,22 +62,34 @@ const WorkerProfile = () => {
                       Service :
                     </h3>
                     <ul className="text-xl font-normal">
-                      <li className="list-disc ml-10">Home Cleaning</li>
-                      <li className="list-disc ml-10">Home Combo Cleaning</li>
-                      <li className="list-disc ml-10">Ac Combo</li>
-                      <li className="list-disc ml-10">Ac Doctor</li>
-                      <li className="list-disc ml-10">Ac Servicing</li>
-                      <li className="list-disc ml-10">Commercial Shifting</li>
-                      <li className="list-disc ml-10">House Shifting</li>
-                      <li className="list-disc ml-10">Pickup Rental</li>
-                      <li className="list-disc ml-10">Car Wash</li>
+                      <li className="list-disc ml-10 italic mb-1">
+                        Home Cleaning
+                      </li>
+                      <li className="list-disc ml-10 italic mb-1">
+                        Home Combo Cleaning
+                      </li>
+                      <li className="list-disc ml-10 italic mb-1">Ac Combo</li>
+                      <li className="list-disc ml-10 italic mb-1">Ac Doctor</li>
+                      <li className="list-disc ml-10 italic mb-1">
+                        Ac Servicing
+                      </li>
+                      <li className="list-disc ml-10 italic mb-1">
+                        Commercial Shifting
+                      </li>
+                      <li className="list-disc ml-10 italic mb-1">
+                        House Shifting
+                      </li>
+                      <li className="list-disc ml-10 italic mb-1">
+                        Pickup Rental
+                      </li>
+                      <li className="list-disc ml-10 italic mb-1">Car Wash</li>
                     </ul>
                   </div>
 
                   <div className="text-xl leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
                     <h3 className="text-xl font-semibold leading-normal text-blueGray-700 mb-2">
                       Rating : <span className="text-xl font-normal"></span>
-                    </h3> 
+                    </h3>
                   </div>
                 </div>
               </div>
@@ -79,8 +98,11 @@ const WorkerProfile = () => {
         </div>
       </section>
       <div>
-        {workerServiceModal && (
+        {workerServiceModal == "details" && (
           <WorkerService handleCloseService={handleCloseService} />
+        )}
+        {workerServiceModal == "payment" && (
+          <WorkerPaymentTable handleCloseService={handleCloseService} />
         )}
       </div>
     </div>
