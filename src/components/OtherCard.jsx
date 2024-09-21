19641ea31954237a603475e95a8143422370ec2d
mdrony5134 <rony830388@gmail.com>
2024-09-21 18:17:06 +0600

    othercard component updated

--- src/components/OtherCard.jsx
+++ src/components/OtherCard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const OtherCard = () => {
   const cardData = [
@@ -22,7 +22,47 @@ const OtherCard = () => {
       alt: "Home Cleaning Image",
       title: "Home Cleaning Service",
     },
+    {
+      img: "/images/gas.webp",
+      alt: "Gas Service Image",
+      title: "Gas Stove Service",
+    },
+    {
+      img: "/images/paint.webp",
+      alt: "Painting Service Image",
+      title: "Painting Services",
+    },
+    {
+      img: "/images/plambing.webp",
+      alt: "Plumbing Service Image",
+      title: "Plumbing Service",
+    },
+    {
+      img: "/images/homecleaning.webp",
+      alt: "Home Cleaning Image",
+      title: "Home Cleaning Service",
+    },
   ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [visibleCard, setVisibleCard] = useState(4)
+
+  useEffect(() => {
+    if(cardData.length <= visibleCard){
+      console.log("first")
+      setVisibleCard(4)
+      setCurrentIndex(0)
+    }
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % cardData.length);
+      setVisibleCard((prevIndex) => (prevIndex + 1));
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [cardData.length, cardData.length === visibleCard]);
+
+  // const visibleCards = 4;
+  const cardSlides = cardData.slice(currentIndex, visibleCard);
+  console.log(currentIndex, visibleCard)
 
   return (
     <div className="py-10">
@@ -34,21 +74,21 @@ const OtherCard = () => {
 
       <div className="w-[90%] mx-auto">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-          {cardData.map((data, index) => (
+          {cardSlides.map((data, index) => (
             <div
               key={index}
-              className="bg-white pt-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow w-[300px] duration-300 flex flex-col overflow-hidden"
+              className="bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-col pt-5"
             >
               <img
                 src={data.img}
                 alt={data.alt}
-                className="rounded-t-lg w-full h-40 hover:scale-110 transition-all ease-linear duration-1000"
+                className="rounded-t-lg w-full h-40 object-cover"
               />
-              <div className="p-4 py-5 flex-grow">
+              <div className="p-4 flex-grow">
                 <h3 className="text-xl font-semibold text-slate-700 mb-2">
                   {data.title}
                 </h3>
-                <p className="text-slate-600 italic">
+                <p className="text-slate-600">
                   Experience the simple pleasures of a world with no cars, and
                   only gentle walks through palm tree forests, and fallen
                   coconuts.
