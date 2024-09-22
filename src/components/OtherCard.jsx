cd3121d21614b3379fc2ec92db32cb33f2a82974
mdrony5134 <rony830388@gmail.com>
2024-09-22 11:54:07 +0600

    OtherCard  component modify and updated

--- src/components/OtherCard.jsx
+++ src/components/OtherCard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const OtherCard = () => {
   const cardData = [
@@ -23,46 +23,40 @@ const OtherCard = () => {
       title: "Home Cleaning Service",
     },
     {
-      img: "/images/gas.webp",
-      alt: "Gas Service Image",
-      title: "Gas Stove Service",
-    },
-    {
       img: "/images/paint.webp",
       alt: "Painting Service Image",
       title: "Painting Services",
     },
-    {
-      img: "/images/plambing.webp",
-      alt: "Plumbing Service Image",
-      title: "Plumbing Service",
-    },
-    {
-      img: "/images/homecleaning.webp",
-      alt: "Home Cleaning Image",
-      title: "Home Cleaning Service",
-    },
   ];
 
+  const itemsToShow = 4;
+  const infiniteData = [...cardData, ...cardData.slice(0, itemsToShow)];
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [visibleCard, setVisibleCard] = useState(4)
+  const sliderRef = useRef(null);
+  const [isTransitioning, setIsTansitioning] = useState(true);
 
   useEffect(() => {
-    if(cardData.length <= visibleCard){
-      console.log("first")
-      setVisibleCard(4)
-      setCurrentIndex(0)
-    }
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % cardData.length);
-      setVisibleCard((prevIndex) => (prevIndex + 1));
-    }, 2000);
+      handleNext();
+    }, 3500);
     return () => clearInterval(interval);
-  }, [cardData.length, cardData.length === visibleCard]);
+  }, [currentIndex]);
 
-  // const visibleCards = 4;
-  const cardSlides = cardData.slice(currentIndex, visibleCard);
-  // console.log(currentIndex, visibleCard)
+  const handleNext = () => {
+    if (sliderRef.current) {
+      setIsTansitioning(true);
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (currentIndex === cardData.length) {
+      setTimeout(() => {
+        setIsTansitioning(false);
+        setCurrentIndex(0);
+      }, 700);
+    }
+  }, [currentIndex, cardData.length]);
 
   return (
     <div className="py-10">
@@ -72,37 +66,60 @@ const OtherCard = () => {
         </h1>
       </div>
 
-      <div className="w-[90%] mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 py-5">
-          {cardSlides.map((data, index) => (
-            <div
-              key={index}
-              className="bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-col  overflow-hidden"
-            >
-              <img
-                src={data.img}
-                alt={data.alt}
-                className="rounded-t-lg w-full h-40 object-cover hover:scale-110 transition-all ease-linear duration-500"
-              />
-              <div className="p-4 flex-grow">
-                <h3 className="text-xl font-semibold text-slate-700 mb-2">
-                  {data.title}
-                </h3>
-                <p className="text-slate-600 italic">
-                  Experience the simple pleasures of a world with no cars, and
-                  only gentle walks through palm tree forests, and fallen
-                  coconuts.
-                </p>
+      <div className="w-[90%] mx-auto overflow-hidden">
+        <div
+          ref={sliderRef}
+          className={`relative flex gap-5 ${
+            isTransitioning ? "transition-transform duration-700" : ""
+          }`}
+          style={{
+            transform: `translateX(-${(100 / itemsToShow) * currentIndex}%)`,
+          }}
+        >
+          {infiniteData.map((data, index) => (
+            <div key={index} className="min-w-[23.7%]">
+              <div className="bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-col overflow-hidden">
+                <img
+                  src={data.img}
+                  alt={data.alt}
+                  className="rounded-t-lg w-full h-40 object-cover hover:scale-110 transition-all ease-linear duration-500"
+                />
+                <div className="p-4 flex-grow">
+                  <h3 className="text-xl font-semibold text-slate-700 mb-2">
+                    {data.title}
+                  </h3>
+                  <p className="text-slate-600 italic">
+                    Experience the simple pleasures of a world with no cars, and
+                    only gentle walks through palm tree forests, and fallen
+                    coconuts.
+                  </p>
+                </div>
               </div>
             </div>
           ))}
         </div>
 
         <div className="flex pt-5 justify-center items-center mt-8 space-x-2">
-          <span className="w-3 h-3 bg-slate-500 rounded-full"></span>
-          <span className="w-3 h-3 bg-slate-300 rounded-full"></span>
-          <span className="w-3 h-3 bg-slate-300 rounded-full"></span>
-          <span className="w-3 h-3 bg-slate-300 rounded-full"></span>
+          <span
+            className={`w-3 h-3 ${
+              currentIndex % itemsToShow === 0 ? "bg-slate-500" : "bg-slate-300"
+            } rounded-full`}
+          ></span>
+          <span
+            className={`w-3 h-3 ${
+              currentIndex % itemsToShow === 1 ? "bg-slate-500" : "bg-slate-300"
+            } rounded-full`}
+          ></span>
+          <span
+            className={`w-3 h-3 ${
+              currentIndex % itemsToShow === 2 ? "bg-slate-500" : "bg-slate-300"
+            } rounded-full`}
+          ></span>
+          <span
+            className={`w-3 h-3 ${
+              currentIndex % itemsToShow === 3 ? "bg-slate-500" : "bg-slate-300"
+            } rounded-full`}
+          ></span>
         </div>
       </div>
     </div>
